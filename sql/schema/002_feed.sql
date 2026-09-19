9bfe41fd13ee490ebdf82298be45b62000269fb4
matthew bootdev <peregoywork@gmail.com>
2026-09-18 20:59:32 -0700

    add last fetched column

--- sql/schema/002_feed.sql
+++ sql/schema/002_feed.sql
@@ -5,7 +5,8 @@ CREATE TABLE feeds (
     updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL,
     url TEXT UNIQUE NOT NULL,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    last_updated TIMESTAMP NULL
 );
 
 -- +goose Down
