e959eeebd020c6221c43c6fbb318d55e7389212e
matthew bootdev <peregoywork@gmail.com>
2026-09-18 20:59:57 -0700

    queries to mark latest and fetch oldest

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -1,12 +1,13 @@
 -- name: CreateFeed :one
-INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
+INSERT INTO feeds (id, created_at, updated_at, name, url, user_id, last_updated)
 VALUES(
     $1, 
     $2, 
     $3, 
     $4, 
     $5, 
-    $6
+    $6,
+    $7
 )
 RETURNING *;
 
@@ -15,4 +16,10 @@ SELECT * FROM feeds WHERE url = $1;
 
 -- name: GetFeeds :many
 SELECT * FROM feeds;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds SET updated_at = NOW(), last_updated = NOW() WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds ORDER BY last_updated NULLS FIRST;
 
