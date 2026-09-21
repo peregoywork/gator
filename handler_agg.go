63efb8eaa4bb6896534202ffe3a420fa30eba131
matthew bootdev <peregoywork@gmail.com>
2026-09-20 21:03:29 -0700

    split existing handler, save posts to DB once scrapped

--- handler_agg.go
+++ handler_agg.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"time"
+	"context"
+	"database/sql"
+
+	"github.com/google/uuid"
 
 	"gator/internal/database"
 )
@@ -32,11 +36,12 @@ func handlerAgg(s *state, cmd command) error {
 
 
 func scrapeFeeds(s *state) error {
-	ctx := context.Background()
-	feed, err := s.db.GetNextFeedToFetch(ctx)
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
 	if err != nil {
-		return log.Errorf("could not get next feeds to fetch: %s", err) 
+		return fmt.Errorf("could not get next feeds to fetch: %s", err) 
 	}
+
+	fmt.Printf("scraping next feed: %s", feed.Name)
 
 	scrapeFeed(s.db, feed)
 
@@ -44,22 +49,52 @@ func scrapeFeeds(s *state) error {
 }
 
 
-func scrapeFeed(db *database.Queries, feed database.Feed) {
-	err = db.MarkFeedFetched(ctx, feed.ID)
+func scrapeFeed(db *database.Queries, feed database.Feed) error {
+	err := db.MarkFeedFetched(context.Background(), feed.ID)
 	if err != nil {
 		return err
 	}
 
-	rssFeed, err := fetchFeed(ctx, feed.Url)
+	rssFeed, err := fetchFeed(context.Background(), feed.Url)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("Feed:")
-	fmt.Printf("%s - %s - %s\n", rssFeed.Channel.Title, rssFeed.Channel.Description, rssFeed.Channel.Link)
 	for _, item := range rssFeed.Channel.Item {
-		fmt.Printf("%+v\n", item.Title)
+		saveRssItemAsPost(db, item, feed.ID)
 	}
+
+	return nil
 }
 
+
+func saveRssItemAsPost(db *database.Queries, item RSSItem, feed_id uuid.UUID) {
+	publishedAt := sql.NullTime{}
+	if t, err := time.Parse(time.RFC1123Z, item.PubDate); err == nil {
+		publishedAt = sql.NullTime{
+			Time: t,
+			Valid: true,
+		}
+	} 
+
+	post, err := db.CreatePost(context.Background(), database.CreatePostParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Title: item.Title,
+		Url: item.Link,
+		Description: sql.NullString{ String: item.Description, Valid: true },
+		PublishedAt: publishedAt,
+		FeedID: feed_id,
+	})
+
+	if err != nil {
+		// errors: ignore if url already exists, log others
+		fmt.Printf("error while creating post: %v\n", err)
+		// unique constraint "posts_url_key"
+		return
+	}
+
+	fmt.Printf("Post Saved: %v\n", post)
+}
 
