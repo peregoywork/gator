6bc870e5f8527912f98399f9b21e544fad7c10c5
matthew bootdev <peregoywork@gmail.com>
2026-09-20 21:01:58 -0700

    add posts table

--- sql/schema/004_posts.sql
+++ sql/schema/004_posts.sql
@@ -6,7 +6,7 @@ CREATE TABLE posts (
     title TEXT NOT NULL,
     url TEXT UNIQUE NOT NULL,
     description TEXT NULL,
-    published_at TIMESTAMP NOT NULL,
+    published_at TIMESTAMP NULL,
     feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
 );
 
